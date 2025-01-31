8176ddac780857016c2c47a9b078b1e07a24aa7d
Mambulya <yashnova.anya@mail.ru>
2025-02-01 02:08:05 +0300

    form_load 1.0 version

--- DatabaseCourse2/Form1.cs
+++ DatabaseCourse2/Form1.cs
@@ -7,18 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DatabaseCourse2.DataSet2TableAdapters;
 
 namespace DatabaseCourse2
 {
     public partial class Form1 : Form
     {
+        DataSet2 ds = new DataSet2();
+        production_normasTableAdapter proTA = new production_normasTableAdapter();
+        worker_experienceTableAdapter weTA = new worker_experienceTableAdapter();
+        worker_infoTableAdapter wiTA = new worker_infoTableAdapter();
+
         public Form1()
         {
             InitializeComponent();
+
         }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             // TODO: данная строка кода позволяет загрузить данные в таблицу "dataSet2.worker_info". При необходимости она может быть перемещена или удалена.
             this.worker_infoTableAdapter.Fill(this.dataSet2.worker_info);
             // TODO: данная строка кода позволяет загрузить данные в таблицу "dataSet2.worker_experience". При необходимости она может быть перемещена или удалена.
