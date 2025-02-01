9b505f29e661fefc67840797672ce40eeaf56264
Mambulya <yashnova.anya@mail.ru>
2025-02-01 23:55:14 +0300

    Sql Procedure form - button FILL is created + the connection is done

--- DatabaseCourse2/CmdForm.cs
+++ DatabaseCourse2/CmdForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,19 @@ namespace DatabaseCourse2
 
         private void btnFill_Click(object sender, EventArgs e)
         {
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-LCHJBBK9;Initial Catalog=uni_serverm1;Integrated Security=True;");
+            try
+            {
+                conn.Open();
 
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MessageBox.Show("Ok");
         }
     }
 }
